3ed09da332e687e670d98d40d0afc1846e210f56
yukta12 <yuktapeswani12@gmail.com>
2019-10-06 01:01:40 +0530

    Links and settings view modified

--- app/chowkidaar/screens/LinksScreen.js
+++ app/chowkidaar/screens/LinksScreen.js
@@ -1,27 +1,15 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { ExpoLinksView } from '@expo/samples';
-
+import HeaderComponent from "../components/HeaderComponent";
 export default function LinksScreen() {
   return (
-    <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-      <ExpoLinksView />
-    </ScrollView>
+      <View>
+        <HeaderComponent/>
+      </View>
   );
 }
 
 LinksScreen.navigationOptions = {
-  title: 'Links',
+  header: null,
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
--- app/chowkidaar/screens/SettingsScreen.js
+++ app/chowkidaar/screens/SettingsScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
+import {View} from "react-native";
+import HeaderComponent from "../components/HeaderComponent";
 import { ExpoConfigView } from '@expo/samples';
-
 export default function SettingsScreen() {
   /**
    * Go ahead and delete ExpoConfigView and replace it with your content;
    * we just wanted to give you a quick view of your config.
    */
-  return <ExpoConfigView />;
+  return(
+      <View>
+        <HeaderComponent/>
+      </View>
+  );
 }
 
 SettingsScreen.navigationOptions = {
-  title: 'app.json',
+  header: null,
 };
+
