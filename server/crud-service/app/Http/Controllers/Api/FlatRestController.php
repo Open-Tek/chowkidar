fc5e9820b5daee4b8a23ac316f839812470be9de
Gaurav-Punjabi <gaurav.bpunjabi@gmail.com>
2019-10-05 21:21:51 +0530

    Added some endpoints in FlatRestController

--- server/crud-service/app/Http/Controllers/Api/FlatRestController.php
+++ server/crud-service/app/Http/Controllers/Api/FlatRestController.php
@@ -2,10 +2,39 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
+use App\Floor;
+use App\Flat;
 use App\Http\Controllers\Controller;
 
 class FlatRestController extends Controller
 {
-    //
+    public function all()
+    {
+        // Fecthing all the flatsfrom the database
+        $flats = Flat::all();
+
+        // Returning the list of flats to the user with response code 200.
+        return response()->json($flats, 200);
+    }
+
+    public function allByFloor($id)
+    {
+        // Checking if any such floor exists or not.
+        Floor::findOrFail($id);
+
+        // Fetching all the flats with the given society id
+        $flats = Flat::where("society_id", $id)
+            ->get();
+
+        // Verifying whether records are fetched or not
+        if($flats) {
+            // If Yes then returning the flats to the user with status code 200.
+            return response()->json($flats, 200);
+        }
+
+        // Otherwise returning 500 to the user coz some issue occurred
+        return response()->json([
+            "message" => "Some internal server error occurred"
+        ], 500);
+    }
 }
